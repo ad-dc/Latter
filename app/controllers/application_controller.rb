8f032be4afbd3568059180285d56563bd737ae55
Jake Mauer <jake@jakemauer.com>
2013-02-12 00:23:27 -0800

    Commented out before filter in application controller that forces setting a password on first login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :needs_password?, :if => :current_player
+  # before_filter :needs_password?, :if => :current_player
   before_filter :set_locale
   protect_from_forgery
 
@@ -27,16 +27,16 @@ class ApplicationController < ActionController::Base
   # - If they have, it carries on
   # - If they have not, it logs them out and redirects them to the change
   #   password page.
-  def needs_password?
-    if current_player and !current_player.changed_password?
-      old_current_player = current_player
-      sign_out :player
-      old_current_player.send(:generate_reset_password_token!)
+  # def needs_password?
+  #   if current_player and !current_player.changed_password?
+  #     old_current_player = current_player
+  #     sign_out :player
+  #     old_current_player.send(:generate_reset_password_token!)
 
-      redirect_to edit_player_password_path(:reset_password_token => old_current_player.reset_password_token)
-      return false
-    else
-      return true
-    end
-  end
+  #     redirect_to edit_player_password_path(:reset_password_token => old_current_player.reset_password_token)
+  #     return false
+  #   else
+  #     return true
+  #   end
+  # end
 end
